1a24d772dca467a8bbeb27af5dddc9e668f106ea
elias <eliaseshetu28@gmail.com>
2024-05-08 20:47:55 +0300

    task 0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import holbertonLoge from './Holberton_Logo.jpg'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="App-header">
+          <img src={holbertonLoge} className='hol_logo' alt=''/>
+          <h1>School dashboard</h1>
+        </div>
       </header>
+      <body>
+        <div className="App-body">
+          <p>Login to access the full dashboard</p>
+        </div>
+      </body>
+      <footer>
+        <div className="App-footer">
+          <p>Copyright 2020 - Holberton School</p>
+        </div>
+      </footer>
     </div>
   );
 }
